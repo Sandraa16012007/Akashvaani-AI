dae42fe32d5ebfa0328636b96c2ff6777d3ac0ed
Aryan Das <aryandas2911@gmail.com>
2026-04-10 14:17:33 +0530

    Frontend connected to supabase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import LandingPage from './pages/LandingPage';
 
 function App() {
-  return (
-    <LandingPage />
-  );
+  return <LandingPage />;
 }
 
 export default App;
