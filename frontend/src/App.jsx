2db604dcbd466417a3e82d069b5921e633e5d4df
Sandraa16012007 <sandrarosaprince@gmail.com>
2026-04-10 13:13:45 +0530

    frontend looks better now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import FeaturesGrid from './components/FeaturesGrid';
-import IntelligenceSection from './components/IntelligenceSection';
-import ProcessSteps from './components/ProcessSteps';
-import Testimonials from './components/Testimonials';
-import AdvocateSection from './components/AdvocateSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-indian-offwhite">
-      <Navbar />
-      <main>
-        <Hero />
-        <FeaturesGrid />
-        <IntelligenceSection />
-        <ProcessSteps />
-        <Testimonials />
-        <AdvocateSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
+    <LandingPage />
   );
 }
 
